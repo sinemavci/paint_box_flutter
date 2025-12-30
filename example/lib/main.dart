7e501b97bd82586ba61f329965ad0b894c2e7e79
sinem.avci <sinem.avci@ekinokssoftware.com>
2025-12-30 17:21:51 +0300

    feat: tiff support

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -19,17 +19,15 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
- final paintEditor1 = PaintEditor();
- final paintEditor2 = PaintEditor();
- File? file;
+  final paintEditor1 = PaintEditor();
+  final paintEditor2 = PaintEditor();
+  File? file;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
+        appBar: AppBar(title: const Text('Plugin example app')),
         body: Column(
           children: [
             SizedBox(
@@ -37,61 +35,83 @@ class _MyAppState extends State<MyApp> {
               child: Stack(
                 children: [
                   Positioned.fill(
-                    child: PaintBoxView(paintEditor: paintEditor1), // Kotlin View
+                    child: PaintBoxView(
+                      paintEditor: paintEditor1,
+                    ), // Kotlin View
                   ),
                   Align(
                     alignment: Alignment.topRight,
-                    child: ElevatedButton(onPressed: () async {
-              await paintEditor1.undo();
-                    }, child: Text("UNDO")),     // Flutter widget
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        await paintEditor1.undo();
+                      },
+                      child: Text("UNDO"),
+                    ), // Flutter widget
                   ),
                   Align(
                     alignment: Alignment.topCenter,
-                    child: ElevatedButton(onPressed: () async {
-              await paintEditor1.redo();
-                    }, child: Text("REDO")),     // Flutter widget
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        await paintEditor1.redo();
+                      },
+                      child: Text("REDO"),
+                    ), // Flutter widget
                   ),
                   Align(
                     alignment: Alignment.topLeft,
-                    child: ElevatedButton(onPressed: () async {
-              await paintEditor1.reset();
-                    }, child: Text("RESET")),     // Flutter widget
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        await paintEditor1.reset();
+                      },
+                      child: Text("RESET"),
+                    ), // Flutter widget
                   ),
                   Align(
                     alignment: Alignment.centerLeft,
-                    child: ElevatedButton(onPressed: () async {
-              FilePickerResult? result = await FilePicker.platform.pickFiles();
-              if (result != null) {
-                File file = File(result.files.single.path!);
-                final bytes = await file.readAsBytes();
-                final base64Result = base64Encode(bytes);
-                await paintEditor1.import(base64Result);
-                } else {
-                    }
-                    }, child: Text("IMPORT")),     // Flutter widget
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        FilePickerResult? result = await FilePicker.platform
+                            .pickFiles();
+                        if (result != null) {
+                          File file = File(result.files.single.path!);
+                          final bytes = await file.readAsBytes();
+                          final base64Result = base64Encode(bytes);
+                          await paintEditor1.import(base64Result);
+                        } else {}
+                      },
+                      child: Text("IMPORT"),
+                    ), // Flutter widget
                   ),
                   Align(
                     alignment: Alignment.center,
-                    child: ElevatedButton(onPressed: () async {
-                final isEnable = await paintEditor1.isEnable();
-                await paintEditor1.setEnable(!isEnable);
-               }, child: Text("ENABLE")),     // Flutter widget
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        final isEnable = await paintEditor1.isEnable();
+                        await paintEditor1.setEnable(!isEnable);
+                      },
+                      child: Text("ENABLE"),
+                    ), // Flutter widget
                   ),
-                   Align(
+                  Align(
                     alignment: Alignment.centerRight,
-                    child: ElevatedButton(onPressed: () async {
-                      final path = await getDownloadsDirectory();
-                      await paintEditor1.export(path!.path, MimeType.jpeg, "exported_imageeee");
-               }, child: Text("EXPORT")),     // Flutter widget
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        final path = await getDownloadsDirectory();
+                        await paintEditor1.export(
+                          path!.path,
+                          MimeType.tif,
+                          "exported_imageeee",
+                        );
+                      },
+                      child: Text("EXPORT"),
+                    ), // Flutter widget
                   ),
                 ],
               ),
             ),
-           Text("Second PaintBoxView"),
-           if (file != null) ...[
-             Image.file(file!),
-           ]
-          
+            Text("Second PaintBoxView"),
+            if (file != null) ...[Image.file(file!)],
+
             // SizedBox(
             //   height: 350,
             //   child: Stack(
@@ -140,9 +160,8 @@ class _MyAppState extends State<MyApp> {
             //     ],
             //   ),
             // ),
-          
           ],
-        )
+        ),
       ),
     );
   }
